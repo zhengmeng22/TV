3e38ea3cde1449112f585a57c720cc46a557ca7c
zhengmeng22 <1284835069@qq.com>
2019-04-22 17:45:23 +0800

    创建项目模块

---
+++ TV-web-admin/src/main/java/com/zm/tv/web/admin/WebAdminApplication.java
@@ -0,0 +1,12 @@
+package com.zm.tv.web.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminApplication.class,args);
+    }
+}
